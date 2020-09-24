7f75b354619bca16962b95198cdda663099dd017
brjatoba92 <69490377+brjatoba92@users.noreply.github.com>
2020-09-24 19:06:51 -0300

    Link do avatar de um usuario no GitHub
    close #8

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+
+    :return:
+    """
+    url = f' https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('brjatoba92'))
